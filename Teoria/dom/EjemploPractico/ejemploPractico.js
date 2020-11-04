3570b590b8baf0b262a8ffe7f39862dfcd7b1565
YPZ-24 <aylinypz@gmail.com>
2020-11-04 15:26:40 -0600

    Ejemplo Practico DOM

---
+++ Teoria/dom/EjemploPractico/ejemploPractico.js
@@ -0,0 +1,33 @@
+//Para traer el cuerpo de la tabla
+//  # para referirse a id
+//  . para referirse a class
+const tableProductos = document.querySelector("#table__products");
+const tableProductosBody = document.querySelector("#table__products__body");
+//Metodo antiguo
+//const tableProductosBody = document.getElementById('table__products__body');
+
+const form__producto = document.querySelector("#form__producto");
+
+form__producto.addEventListener("submit", submitForm);
+
+
+function submitForm(e){
+    //Traigo el valor de los input
+    const producto = document.querySelector('#producto').value;
+    const precio = document.querySelector('#precio').value;
+    //Creo una nueva fila
+    const tr = document.createElement('tr');
+    //Creo las celdas
+    const tdProducto = document.createElement('td');
+    const tdPrecio = document.createElement('td');
+    //Les pongo el texto
+    tdProducto.textContent = producto;
+    tdPrecio.textContent = precio;
+    //Meto las celdas en la fila
+    tr.appendChild(tdProducto);
+    tr.appendChild(tdPrecio);
+    //Meto la fila en el cuerpo de la tabla
+    tableProductosBody.appendChild(tr);
+
+    e.preventDefault();
+}
